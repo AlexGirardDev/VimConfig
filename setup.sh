2ea8c9824f2345251885a9b9095d90f425fd266d
Alex Girard <me@alexgirard>
2020-10-11 18:49:46 +0100

    adding setup script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#Install plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#setup vim
+mkdir ~/nvim
+rm ~/nvim/init.vim
+ln ~/dotfiles/VimConfig/init.vim ~/nvim/init.vim
+rm ~/.vimrc
+ln ~/dotfiles/VimConfig/.vimrc ~/.vimrcm
+
+
+
